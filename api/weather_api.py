2232e809b353b76e2f5cb08ae8699e314c5ba586
cheburek4535 <alexblazeice@gmail.com>
2025-07-26 23:17:08 +0300

    init  commit 3

--- api/weather_api.py
+++ api/weather_api.py
@@ -7,7 +7,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
+weather_api_key = "2a27c5db9a8bd093c75fc9f50314845d"
 def get_weather(city: str, api_key: str) -> dict:
     """
     Получает текущую погоду для указанного города
@@ -31,15 +31,49 @@ def get_weather(city: str, api_key: str) -> dict:
 
         # Извлекаем нужные данные
         return {
-            'city': data['name'],
-            'temp': data['main']['temp'],
-            'feels_like': data['main']['feels_like'],
-            'description': data['weather'][0]['description'],
-            'humidity': data['main']['humidity']
+            '🌆Город': data['name'],
+            '🌡️Температура': data['main']['temp'],
+            '🤗Ощущается как': data['main']['feels_like'],
+            '🌥️Общее состояние': data['weather'][0]['description'],
+            '💧Влажность': data['main']['humidity']
         }
     except requests.exceptions.RequestException as e:
         logger.error(f"Ошибка запроса погоды: {e}")
         return None
 
-print(get_weather("Moscow", "2a27c5db9a8bd093c75fc9f50314845d"))
+print(get_weather("London", weather_api_key))
 
+
+# import logging
+# import requests
+#
+# logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+#
+# logger = logging.getLogger(__name__)
+#
+#
+# def get_weather(city: str, api_key: str) -> dict:
+#     """
+#     Получает текущую погоду для указанного города
+#     :param city: Название города (например "Москва")
+#     :param api_key: Ваш API-ключ OpenWeatherMap
+#     :return: Словарь с данными о погоде
+#     """
+#     base_url = "http://api.openweathermap.org/data/2.5/weather?"
+#
+#     params = {
+#         "q": city,
+#         "appid": api_key,
+#         "units": "metric",
+#         "lang": "ru"
+#     }
+#
+#     try:
+#         response = requests.get(base_url, params=params)
+#         response.raise_for_status()
+#
+#         data = response.json()
+#
+#         return {
+#
+#         }
