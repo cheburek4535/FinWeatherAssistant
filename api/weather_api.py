3c53d5c71fc1c7bd29ec97c649b372a7f04eba84
cheburek4535 <alexblazeice@gmail.com>
2025-07-26 19:58:17 +0300

    init  commit 3

---
+++ api/weather_api.py
@@ -0,0 +1,45 @@
+import requests
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+def get_weather(city: str, api_key: str) -> dict:
+    """
+    Получает текущую погоду для указанного города
+    :param city: Название города (например "Москва")
+    :param api_key: Ваш API-ключ OpenWeatherMap
+    :return: Словарь с данными о погоде
+    """
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric',  # для получения в °C
+        'lang': 'ru'  # русский язык ответа
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()  # Проверка ошибок HTTP
+
+        data = response.json()
+
+        # Извлекаем нужные данные
+        return {
+            'city': data['name'],
+            'temp': data['main']['temp'],
+            'feels_like': data['main']['feels_like'],
+            'description': data['weather'][0]['description'],
+            'humidity': data['main']['humidity']
+        }
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Ошибка запроса погоды: {e}")
+        return None
+
+print(get_weather("Moscow", "2a27c5db9a8bd093c75fc9f50314845d"))
+
